68db1cb4ef74e40dd0893363ad89059e53d69295
charlesancheta <cancheta@ualberta.ca>
2019-12-31 20:07:03 -0700

    i want to die?? why am i still working on this

--- newyear2020/happy2020.ino
+++ newyear2020/happy2020.ino
@@ -1,12 +1,16 @@
 #include <Arduino.h>
 bool is2020 = false;
+int year2019[4] = {2,0,1,9};
+int year2020[4] = {2,0,2,0};
 
 void clearLEDs() {
   // diplay is Common Cathode => HIGH = OFF
-  for (int i = 42; i <= 45; i++) {
-    digitalWrite(i,HIGH);
+  for (int i = 42; i < 46; i++) {
+    digitalWrite(i, HIGH);
   }
+
 }
+
 void setup() {
   // initializes pinMode for pins 10 and 12 and pull up resistors
   pinMode(10,INPUT); digitalWrite(10,HIGH);
@@ -20,85 +24,35 @@ void setup() {
   for (int i = 42; i <= 45; i++) {
     pinMode(i,OUTPUT);
   }
+  
 }
+
 void writeOn7Seg(int digit, int number) {
                     //D1,D2,D3,D4
   int digitPins[4] = {42,43,44,45};
                   //A, B, C, D, E, F, G, dp
   int segPins[8] = {51,53,49,47,46,52,50,48};
-  int numZero[8] = {1,1,1,1,1,1,0,0};
-  int numOne[8] = {0,1,1,0,0,0,0,0};
-  int numTwo[8] = {1,1,0,1,1,0,1,0};
-  int numThree[8] = {1,1,1,1,0,0,1,0};
-  int numFour[8] = {0,1,1,0,0,1,1,0};
-  int numFive[8] = {1,0,1,1,0,1,1,0};
-  int numNine[8] = {1,1,1,1,0,1,1,0};
+  int numDec[10] = {63,6,91,79,102,109,125,7,127,111};
   clearLEDs();
-  digitalWrite(digitPins[digit-1], LOW);
-  switch (number) {
-    case 0:
-      for (int i = 0; i < 8; i++) {
-        digitalWrite(segPins[i], numZero[i]);
-      }
-      break;
-    case 1:
-      for (int i = 0; i < 8; i++) {
-        digitalWrite(segPins[i], numOne[i]);
-      }
-      break;
-    case 2:
-      for (int i = 0; i < 8; i++) {
-        digitalWrite(segPins[i], numTwo[i]);
-      }
-      break;
-    case 3:
-      for (int i = 0; i < 8; i++) {
-        digitalWrite(segPins[i], numThree[i]);
-      }
-      break;
-    case 4:
-      for (int i = 0; i < 8; i++) {
-        digitalWrite(segPins[i], numFour[i]);
-      }
-      break;
-    case 5:
-      for (int i = 0; i < 8; i++) {
-        digitalWrite(segPins[i], numFive[i]);
-      }
-      break;
-    case 9:
-      for (int i = 0; i < 8; i++) {
-        digitalWrite(segPins[i], numNine[i]);
-      }
-      break;
-    default:
-      for (int i = 0; i < 8; i++) {
-        digitalWrite(segPins[i], HIGH);
-      }
-  } 
+  int numDisplayed = numDec[number];
+  digitalWrite(digitPins[digit], LOW);
+  for (int i = 0; i < 8; i++) {
+    if (numDisplayed & 1) {
+      digitalWrite(segPins[i], HIGH);
+    } else {
+      digitalWrite(segPins[i], LOW);
+    }
+    numDisplayed >>= 1;
+  }
+  
 
 }
 
-void write2019() {
-  writeOn7Seg(1,2);
-  delay(1);
-  writeOn7Seg(2,0);
-  delay(1);
-  writeOn7Seg(3,1);
-  delay(1);
-  writeOn7Seg(4,9);
-  delay(1);
-}
-
-void write2020() {
-  writeOn7Seg(1,2);
-  delay(1);
-  writeOn7Seg(2,0);
-  delay(1);
-  writeOn7Seg(3,2);
-  delay(1);
-  writeOn7Seg(4,0);
-  delay(1);
+void write4digits(int num[]) {
+  for (int i = 0; i < 4 ; i++) {
+    writeOn7Seg(i,num[i]);
+    delay(1);
+  }
 }
 
 void playNote(int note,int dur) {
@@ -110,13 +64,37 @@ void playNote(int note,int dur) {
   int delayCount = 0;
   // displays 2020 while the tone is playing
   while (delayCount < dur) {
-    write2020();
+    write4digits(year2020);
     delayCount += 4;
   }
 }
 
+void playLick(int beat[], int tempo) {
+  int LEDArray[5] = {22,23,24,25,26};
+  int note[8] = {4,5,8,6,4,5,3,4};
+  // because the lights are remapped
+  int LEDs[8] = {1,2,3,4,1,2,0,1};
+  for (int i = 0; i < 8; i++) {
+    digitalWrite(LEDArray[LEDs[i]],HIGH);
+    playNote(note[i],beat[i]*tempo);
+    digitalWrite(LEDArray[LEDs[i]],LOW);
+  }
+}
 
-void playAuldLangSyne() {
+void lick(bool isWack) {
+  // pa da da da dum doo wah
+  int beat[8] = {4,2,4,2,1,5,4,6};
+  int beatWack1[8] = {1,1,1,1,0,1,1,0};
+  int beatWack2[8] = {1,1,1,2,0,2,1,5};
+  if (isWack) {
+    playLick(beatWack1,105);
+    playLick(beatWack2,105);
+  } else {
+    playLick(beat,63);
+  }
+}
+
+void playMelody(int beat[], int tempo) {
   int LEDArray[8] = {22,23,24,25,26,27,28,29};
   // The notes of the melody with the frequencies defined
   // in the playNote function
@@ -124,61 +102,45 @@ void playAuldLangSyne() {
                   7,6,5,5,3,4,3,4,5,4,3,1,1,0,3,
                   7,6,5,5,3,4,3,4,7,6,5,5,6,7,
                   7,6,5,5,3,4,3,4,5,4,3,1,1,0,};
-  // the durations of the notes in terms of eighth notes
+  for (int i = 0; i < 58; i++) {
+    digitalWrite(LEDArray[note[i]], HIGH);
+    playNote(note[i],beat[i]*tempo);
+    digitalWrite(LEDArray[note[i]], LOW);
+  }
+}
+
+void playAuldLangSyne(bool isWack) {
   int beat[58] = {2,3,1,2,2,3,1,2,1,1,3,1,2,2,6,
                   2,3,1,2,2,3,1,2,1,1,3,1,2,2,6,
                   2,3,1,2,2,3,1,2,2,3,1,2,2,6,
                   2,3,1,2,2,3,1,2,1,1,3,1,2,2,};
-  for (int i = 0; i < 58; i++) {
-    digitalWrite(LEDArray[note[i]], HIGH);
-    // one eighth note in 133.5 bpm is 225 ms
-    playNote(note[i],beat[i]*225);
-    digitalWrite(LEDArray[note[i]], LOW);
-  }
-}
-
-void playWack() {
-  // Auld Lang Syne in 11/8 Time Signature
-  int LEDArray[8] = {22,23,24,25,26,27,28,29};
-  int note[58] = {0,3,2,3,5,4,3,4,5,4,3,3,5,6,7,
-                  7,6,5,5,3,4,3,4,5,4,3,1,1,0,3,
-                  7,6,5,5,3,4,3,4,7,6,5,5,6,7,
-                  7,6,5,5,3,4,3,4,5,4,3,1,1,0,};
-  int beat[58] = {2,2,1,1,1,2,1,1,1,1,2,1,1,1,5,
+  int wack[58] = {1,2,1,1,1,2,1,1,1,1,2,1,1,1,5,
                   1,2,1,1,1,2,1,1,1,1,2,1,1,1,5,
-                  1,2,1,1,1,2,1,1,1,2,1,1,1,5,
-                  1,2,1,1,1,2,1,1,1,1,2,1,1,1,};
-  for (int i = 0; i < 58; i++) {
-    digitalWrite(LEDArray[note[i]], HIGH);
-    playNote(note[i],beat[i]*211);
-    digitalWrite(LEDArray[note[i]], LOW);
+                  1,2,1,1,1,2,1,2,1,2,1,1,1,5,
+                  1,2,1,1,1,2,1,1,1,1,2,1,1,1};
+  if (isWack) {
+    playMelody(wack,211);
+  } else { 
+    playMelody(beat,225);
   }
+  lick(isWack);
 }
 
-void lick() {
-  // pa da da da dum doo wah
-  int LEDArray[5] = {22,23,24,25,26};
-  int note[8] = {4,5,8,6,4,5,3,4};
-  int beat[8] = {4,2,4,2,1,5,4,6};
-  // because the lights are remapped
-  int LEDs[8] = {1,2,3,4,1,2,0,1};
-  for (int i = 0; i < 8; i++) {
-    digitalWrite(LEDArray[LEDs[i]],HIGH);
-    playNote(note[i],beat[i]*63);
-    digitalWrite(LEDArray[LEDs[i]],LOW);
-  }
-}
 void countDown() {
   for (int i = 5; i > 0; i--){
-    writeOn7Seg(2,i); delay(700);
-    clearLEDs(); delay(300);
+    writeOn7Seg(1,i); 
+    if (digitalRead(10) == HIGH || digitalRead(12) == HIGH) {
+      delay(700); clearLEDs(); delay(300);
+    } else {
+      delay(200);
+    }
   }
 }
 void flash2020() {
   for (int i = 0; i < 3; i++) {
     int delayCount = 0;
     while (delayCount < 300) {
-      write2020();
+      write4digits(year2020);
       delayCount += 4;
     }
     clearLEDs();
@@ -187,9 +149,9 @@ void flash2020() {
 }
 void loop() {
   if (!is2020) {
-    write2019();
+    write4digits(year2019);
   } else {
-    write2020();
+    write4digits(year2020);
   }
   if (digitalRead(10) == LOW || digitalRead(12) == LOW) {
     bool isWack = false;
@@ -204,12 +166,7 @@ void loop() {
     For auld lang syne, my jo, for auld lang syne,
     We'll tak a cup of kindness yet, for auld lang syne.
     */
-    if (isWack) {
-      playWack();
-    } else {
-      playAuldLangSyne();
-    }
-    lick();
+    playAuldLangSyne(isWack);
   }
   // Happy New Year man
 }
--- newyear2020/sipopo-files/hello.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-  std::cout << "Sigma balls" << std::endl;
-  return 0;
-}

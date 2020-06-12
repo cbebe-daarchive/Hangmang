3d8103325c60d755f4fdb0a1e9fbbdce42a9c5e9
Charles Ancheta <cancheta@ualberta.ca>
2020-06-11 19:37:56 -0600

    big bruh

--- hangmang/game.cpp
+++ hangmang/game.cpp
@@ -1,12 +1,43 @@
+#include <Arduino.h>
+#include <stdlib.h>
+#include <string.h>
+
 const int maxWrongGuesses = 6;
+
+char getInput()
+{
+  return 'A';
+}
+
+void correctGuess(char guessedChar)
+{
+}
 
 void game()
 {
+  bool guessed = false;
   char guessedLetters[26];
   int guesses = 0, wrongGuesses = 0;
-  char *word = "TESTING";
+  const char *word = "TESTING";
 
-  while (wrongGuesses < maxWrongGuesses)
+  while (wrongGuesses < maxWrongGuesses && !guessed)
   {
+    // implement getting user input
+    char guessedChar = getInput();
+    if (strchr(word, guessedChar) == NULL)
+      wrongGuesses++;
+    else
+      correctGuess(guessedChar); // display correct char
+
+    guessedLetters[guesses++] = guessedChar;
+  }
+
+  if (guessed)
+  {
+    // you won waw
+  }
+  else
+  {
+    // you los rip
   }
 }
